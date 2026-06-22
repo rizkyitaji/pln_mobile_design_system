e33600c9185a130842be1af03316473d269a0f3a
Fawwaz Daffa <fawwaz.sayyaf@iconpln.co.id>
2026-06-22 11:57:29 +0700

    chore: add assets and adjust widget

---
+++ lib/src/widgets/inputs/fields/dropdown_select_field.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pln_mobile_design_system/pln_mobile_design_system.dart';
+
+class AppDropdownSelectField extends StatelessWidget {
+  final String label;
+  final String value;
+  final bool required;
+  final VoidCallback onTap;
+
+  const AppDropdownSelectField({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.onTap,
+    this.required = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AppFieldLabel(label: label, required: required),
+        const SizedBox(height: AppSizes.s8),
+        InkWell(
+          onTap: onTap,
+          borderRadius: AppRadius.rounded8,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppSizes.s16,
+              vertical: AppSizes.s12,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: AppRadius.rounded8,
+              border: Border.all(color: AppColors.border),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  value,
+                  style: context.textTheme.bodyMedium.copyWith(
+                    color: AppColors.textHeading,
+                  ),
+                ),
+                const Icon(
+                  Icons.keyboard_arrow_down_rounded,
+                  color: AppColors.iconDisabled,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
